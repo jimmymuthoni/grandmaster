4924fa47dd93b375031cf0e6191362f08ee44d69
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-11-29 16:00:43 +0300

    constant variables

---
+++ include/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace ChessConstants{
+    const int BOARD_SIZE = 8;
+    const int SQUARE_SIZE = 50;
+    const int WINDOW_SIZE = BOARD_SIZE * SQUARE_SIZE;
+}
+
+#endif
