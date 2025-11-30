cfc52ff7d23718289cd3f3848fb927fb5873252c
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-11-30 15:31:26 +0300

    resolved importation error

--- include/piece.h
+++ include/piece.h
@@ -5,8 +5,8 @@
 
 class Piece {
 public:
-    enum class Type { KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN, NONE };
-    enum class Color { WHITE, BLACK, NONE };
+    enum class Type {KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN, NONE};
+    enum class Color {WHITE, BLACK, NONE};
 
     Piece();
     Piece(Type type, Color color);
