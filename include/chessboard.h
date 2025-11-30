8e3d6adaa507556b02d427f2a083b655596bd697
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-11-30 16:10:37 +0300

    header file for chessboard

---
+++ include/chessboard.h
@@ -0,0 +1,56 @@
+#ifndef CHESSBOARD_H
+#define CHESSBOARD_H
+
+#include <QtWidgets>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QMediaPlayer>
+#include <vector>
+
+class ChessBoard : public QWidget{
+
+    Q_OBJECT
+    public:
+        explicit ChessBoard(QWidget *parent = nullptr);
+        ~ChessBoard();
+
+    private:
+        void initializeBoard();
+        void setupInitialPosition();
+        QString getPieceSymbol(char piece);
+        QString getPieceSvgPath(char piece);
+
+        std::vector<std::vector<QPushButton*>> squares;
+        QPushButton* selectedPiece;
+        QGridLayout *layout;
+        QString currentTurn;
+
+        QMediaPlayer* moveSound;
+
+        bool isValidMove(int fromRow, int fromCol, int toRow, int toCol);
+        bool isValidPawnMove(int fromRow,int fromCol, int toRow, int toCol);
+        bool isValidRookMove(int fromRow, int fromCol, int toRow,int toCol);
+        bool isValidKnightMove(int fromRow, int fromCol, int toRow, int toCol);
+        bool isValidBishopMove(int fromRow, int fromCol, int toRow, int toCol);
+        bool isValidQueenMove(int fromRow, int fromCol, int toRow, int toCol);
+        bool isValidKingMove(int fromRow, int fromCol, int toRow, int toCol);
+        bool isPathClear(int fromRow, int fromCol, int toRow, int toCol);
+        char getPieceAT(int row, int col);
+
+        std::vector<std::vector<char>> initilPosition = {
+            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+            {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
+
+        };
+
+    private slots:
+        void squareClicked();
+};
+
+#endif
