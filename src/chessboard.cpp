bdd51058a3c0f3981e79f917cf9f36afe763789f
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-12-01 14:50:29 +0300

    initialized the cess board

--- src/chessboard.cpp
+++ src/chessboard.cpp
@@ -8,7 +8,7 @@
 QMap<QPushButton*, QString> squareOriginalStyles;
 
 
-//initial stste of chessbord during new game
+//structure of chessboard
 ChessBoard::ChessBoard(QWidget *parent) : QWidget(parent),selectedPiece(nullptr),currentTurn("white"){
     layout = new QGridLayout(this);
     layout->setSpacing(0);
@@ -18,6 +18,32 @@ ChessBoard::ChessBoard(QWidget *parent) : QWidget(parent),selectedPiece(nullptr)
     moveSound-> setSource(QUrl("grc:/assets/sounds/move-self.mp3"));
     initializeBoard();
     setupInitialPosition();
+
+}
+
+//initilizing the chessboard
+void ChessBoard::initializeBoard(){
+    squares.resize(8, std::vector<QPushButton*>(8));
+
+    for(int row=0; row<8; row++){
+        for (int col=0;col<8;col++){
+        QPushButton* square = new QPushButton(this);
+        square->setFixedSize(50,50);
+        square->setProperty("row",row);
+        square->setProperty("col",col);
+
+        QString style = ((row + col) % 2 == 0)
+            ? "background-color: #7c4c3e; font-size: 32px;"
+            : "background-color: #512a2a; font-size: 32px;";
+        square-> setStyleSheet(style);
+        squareOriginalStyles[square] = style;
+
+        connect(square, &QPushButton::clicked, this, &ChessBoard::squareClicked);
+        squares[row][col] = square;
+        layout->addWidget(square,row,col);
+        }
+
+    }
 
 }
 
